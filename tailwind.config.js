4c910dc10ca3f67c4283e23ba73d06fb5b209497
hsnaregia <139281016+hsnaregia@users.noreply.github.com>
2024-09-30 15:08:19 +0330

    making the slider of the APP

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Neon Night Palette
+        neonNight: {
+          midnightBlue: '#1A1A40',  // Background
+          electricPink: '#FF477E',   // Main action buttons
+          aquaGlow: '#00F5D4',       // Progress bar, volume slider, hover effects
+          slateGrey: '#3E3E55',      // Icons, secondary text, inactive controls
+        },
+      }
+    },
   },
   plugins: [],
 }
